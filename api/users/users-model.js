99af82ddae36f01fb6eebd5433aa737a45f64f03
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-06-22 18:46:21 -0600

    users model complete

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,9 @@ function find() {
       }
     ]
    */
+  return db('users as u')
+    .join('roles as r', 'u.role', '=', 'r.id')
+    .select('u.user_id', 'u.username', 'r.name as role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db('users a u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.name as role_name', 'u.password')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +54,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.name as role_name')
+    .where('u.user_id', user_id)
+    .first()
 }
 
 /**
